810d696a1a2a296d4d010f11cfb2a095832b811e
vanminh <minhp4541@gmail.com>
2022-06-03 18:15:50 +0700

    fix docgia

--- AppDocGia.Designer.cs
+++ AppDocGia.Designer.cs
@@ -43,6 +43,7 @@ namespace QUANLITHUVIENWINFORM
             this.label2 = new System.Windows.Forms.Label();
             this.txtGhichu = new System.Windows.Forms.TextBox();
             this.dgvSach = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.ptbSearch)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSach)).BeginInit();
             this.SuspendLayout();
@@ -51,10 +52,9 @@ namespace QUANLITHUVIENWINFORM
             // 
             this.lbName.AutoSize = true;
             this.lbName.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbName.Location = new System.Drawing.Point(787, 239);
-            this.lbName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbName.Location = new System.Drawing.Point(590, 194);
             this.lbName.Name = "lbName";
-            this.lbName.Size = new System.Drawing.Size(67, 23);
+            this.lbName.Size = new System.Drawing.Size(53, 18);
             this.lbName.TabIndex = 56;
             this.lbName.Text = "Mã thẻ";
             // 
@@ -63,10 +63,9 @@ namespace QUANLITHUVIENWINFORM
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.label1.Location = new System.Drawing.Point(807, 96);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(605, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 40);
+            this.label1.Size = new System.Drawing.Size(230, 33);
             this.label1.TabIndex = 54;
             this.label1.Text = "Thư Viện HCMUTE";
             // 
@@ -74,10 +73,9 @@ namespace QUANLITHUVIENWINFORM
             // 
             this.lbNgayMuon.AutoSize = true;
             this.lbNgayMuon.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNgayMuon.Location = new System.Drawing.Point(732, 297);
-            this.lbNgayMuon.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNgayMuon.Location = new System.Drawing.Point(549, 241);
             this.lbNgayMuon.Name = "lbNgayMuon";
-            this.lbNgayMuon.Size = new System.Drawing.Size(122, 23);
+            this.lbNgayMuon.Size = new System.Drawing.Size(97, 18);
             this.lbNgayMuon.TabIndex = 57;
             this.lbNgayMuon.Text = "Ngày phải trả";
             // 
@@ -89,10 +87,9 @@ namespace QUANLITHUVIENWINFORM
             this.btnXacnhan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnXacnhan.Font = new System.Drawing.Font("Bahnschrift SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXacnhan.ForeColor = System.Drawing.Color.White;
-            this.btnXacnhan.Location = new System.Drawing.Point(709, 507);
-            this.btnXacnhan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnXacnhan.Location = new System.Drawing.Point(532, 412);
             this.btnXacnhan.Name = "btnXacnhan";
-            this.btnXacnhan.Size = new System.Drawing.Size(203, 53);
+            this.btnXacnhan.Size = new System.Drawing.Size(152, 43);
             this.btnXacnhan.TabIndex = 58;
             this.btnXacnhan.Text = "Yêu cầu mượn";
             this.btnXacnhan.UseVisualStyleBackColor = false;
@@ -102,10 +99,9 @@ namespace QUANLITHUVIENWINFORM
             // 
             this.cbMathe.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbMathe.FormattingEnabled = true;
-            this.cbMathe.Location = new System.Drawing.Point(873, 236);
-            this.cbMathe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbMathe.Location = new System.Drawing.Point(655, 192);
             this.cbMathe.Name = "cbMathe";
-            this.cbMathe.Size = new System.Drawing.Size(281, 30);
+            this.cbMathe.Size = new System.Drawing.Size(212, 25);
             this.cbMathe.TabIndex = 60;
             // 
             // btnHuy
@@ -116,10 +112,9 @@ namespace QUANLITHUVIENWINFORM
             this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHuy.Font = new System.Drawing.Font("Bahnschrift SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHuy.ForeColor = System.Drawing.Color.White;
-            this.btnHuy.Location = new System.Drawing.Point(993, 507);
-            this.btnHuy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHuy.Location = new System.Drawing.Point(745, 412);
             this.btnHuy.Name = "btnHuy";
-            this.btnHuy.Size = new System.Drawing.Size(203, 53);
+            this.btnHuy.Size = new System.Drawing.Size(152, 43);
             this.btnHuy.TabIndex = 59;
             this.btnHuy.Text = "Thoát";
             this.btnHuy.UseVisualStyleBackColor = false;
@@ -128,10 +123,9 @@ namespace QUANLITHUVIENWINFORM
             // txtSearch
             // 
             this.txtSearch.Font = new System.Drawing.Font("Bahnschrift SemiBold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearch.Location = new System.Drawing.Point(316, 66);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Location = new System.Drawing.Point(273, 91);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(287, 23);
+            this.txtSearch.Size = new System.Drawing.Size(216, 20);
             this.txtSearch.TabIndex = 64;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
@@ -146,10 +140,9 @@ namespace QUANLITHUVIENWINFORM
             // 
             this.ptbSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ptbSearch.Image = global::QUANLITHUVIENWINFORM.Properties.Resources._49116;
-            this.ptbSearch.Location = new System.Drawing.Point(603, 66);
-            this.ptbSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ptbSearch.Location = new System.Drawing.Point(488, 91);
             this.ptbSearch.Name = "ptbSearch";
-            this.ptbSearch.Size = new System.Drawing.Size(34, 24);
+            this.ptbSearch.Size = new System.Drawing.Size(26, 20);
             this.ptbSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ptbSearch.TabIndex = 65;
             this.ptbSearch.TabStop = false;
@@ -159,11 +152,11 @@ namespace QUANLITHUVIENWINFORM
             this.dtp_ngayMuon.CustomFormat = "dd/MM/yyyy";
             this.dtp_ngayMuon.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_ngayMuon.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtp_ngayMuon.Location = new System.Drawing.Point(873, 289);
-            this.dtp_ngayMuon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtp_ngayMuon.Location = new System.Drawing.Point(655, 235);
+            this.dtp_ngayMuon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtp_ngayMuon.Name = "dtp_ngayMuon";
             this.dtp_ngayMuon.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.dtp_ngayMuon.Size = new System.Drawing.Size(281, 28);
+            this.dtp_ngayMuon.Size = new System.Drawing.Size(212, 24);
             this.dtp_ngayMuon.TabIndex = 66;
             this.dtp_ngayMuon.Value = new System.DateTime(2022, 6, 3, 0, 27, 52, 0);
             // 
@@ -171,37 +164,46 @@ namespace QUANLITHUVIENWINFORM
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(787, 345);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(590, 280);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 23);
+            this.label2.Size = new System.Drawing.Size(57, 18);
             this.label2.TabIndex = 67;
             this.label2.Text = "Ghi chú";
             // 
             // txtGhichu
             // 
-            this.txtGhichu.Location = new System.Drawing.Point(871, 345);
-            this.txtGhichu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtGhichu.Location = new System.Drawing.Point(653, 280);
             this.txtGhichu.Name = "txtGhichu";
-            this.txtGhichu.Size = new System.Drawing.Size(281, 22);
+            this.txtGhichu.Size = new System.Drawing.Size(212, 20);
             this.txtGhichu.TabIndex = 68;
             // 
             // dgvSach
             // 
+            this.dgvSach.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgvSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSach.Location = new System.Drawing.Point(39, 154);
+            this.dgvSach.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2});
+            this.dgvSach.Location = new System.Drawing.Point(2, 119);
+            this.dgvSach.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvSach.Name = "dgvSach";
+            this.dgvSach.RowHeadersVisible = false;
             this.dgvSach.RowHeadersWidth = 51;
             this.dgvSach.RowTemplate.Height = 24;
-            this.dgvSach.Size = new System.Drawing.Size(515, 150);
+            this.dgvSach.Size = new System.Drawing.Size(511, 450);
             this.dgvSach.TabIndex = 69;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 50;
             // 
             // AppDocGia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1257, 704);
+            this.ClientSize = new System.Drawing.Size(943, 572);
             this.Controls.Add(this.dgvSach);
             this.Controls.Add(this.txtGhichu);
             this.Controls.Add(this.label2);
@@ -216,7 +218,6 @@ namespace QUANLITHUVIENWINFORM
             this.Controls.Add(this.btnXacnhan);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "AppDocGia";
             this.Text = "Độc giả";
@@ -243,5 +244,6 @@ namespace QUANLITHUVIENWINFORM
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtGhichu;
         private System.Windows.Forms.DataGridView dgvSach;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column2;
     }
 }
